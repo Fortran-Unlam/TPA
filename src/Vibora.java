ed8bc12d64e856a412024dec910ceb495d0dea48
joniborda <jonathanmatiasborda@gmail.com>
2018-09-30 22:42:24 -0300

    Doc

--- src/Vibora.java
+++ src/Vibora.java
@@ -3,24 +3,47 @@ public class Vibora {
 	private int frutasComidas;
 	private CuerpoVibora[] cuerpos;
 	private int sentido;
-	
+
+	/**
+	 * Mueve la vibora hacia el ultimo sentido. Para moverse se elimina la cola y se
+	 * agrega un cuerpo delante de la cabeza
+	 */
 	public void mover() {
-		
+
 	}
-	
+
+	/**
+	 * Mueve la vibora habia el sentido dado
+	 * 
+	 * @param sentido
+	 */
 	public void mover(int sentido) {
-		
+
 	}
-	
+
+	/**
+	 * Verifica si choca con algo del mapa
+	 * 
+	 * @return true si choca
+	 */
 	public boolean choque() {
+		// ver si devolver true o devolver una entidad con la que colisiona
 		return false;
 	}
-	
+
+	/**
+	 * No elimina la cola al moverse
+	 */
 	public void crecer() {
-		
+
 	}
-	
-	public int getPuntuacion() {
+
+	/**
+	 * Devuelve la cantidad de frutas comidas
+	 * 
+	 * @return El numero de frutas comidas
+	 */
+	public int getFrutasComidas() {
 		return this.frutasComidas;
 	}
 }
