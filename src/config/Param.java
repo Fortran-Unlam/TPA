9248e24848413db352aa3e0e169415060a410688
jborda <jborda@jus.gov.ar>
2018-10-01 07:55:02 -0300

    Agrego clase abstracta param donde se van a guardar los parametros globales.

---
+++ src/config/Param.java
@@ -0,0 +1,9 @@
+package config;
+
+public abstract class Param {
+
+	public static final int POSICION_ESTE = 0;
+	public static final int POSICION_SUR = 1;
+	public static final int POSICION_OESTE = 2;
+	public static final int POSICION_NORTE = 3;
+}
