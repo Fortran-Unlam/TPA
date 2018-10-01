f0de22ebceaf0f72168d3de1e17c9aeae7a27310
joniborda <jonathanmatiasborda@gmail.com>
2018-09-30 22:35:37 -0300

    Creo proyecto y agrego clase vibora

---
+++ src/Vibora.java
@@ -0,0 +1,26 @@
+
+public class Vibora {
+	private int frutasComidas;
+	private CuerpoVibora[] cuerpos;
+	private int sentido;
+	
+	public void mover() {
+		
+	}
+	
+	public void mover(int sentido) {
+		
+	}
+	
+	public boolean choque() {
+		return false;
+	}
+	
+	public void crecer() {
+		
+	}
+	
+	public int getPuntuacion() {
+		return this.frutasComidas;
+	}
+}
