fe3a319c5a0fe97ea57185aed24bb6803847ec08
joniborda <jonathanmatiasborda@gmail.com>
2018-10-09 22:59:00 -0300

    mover vibora

--- src/core/Vibora.java
+++ src/core/Vibora.java
@@ -2,16 +2,53 @@ package core;
 
 import java.util.List;
 
+import config.Param;
+
 public class Vibora {
 	private int frutasComidas;
 	private List<CuerpoVibora> cuerpos;
 	private int sentido;
+	private CuerpoVibora cabeza;
+
+	/**
+	 * Consigue la cabeza que está en la ultima posicion
+	 * Asume que la cabeza se actualiza en cada movimiento
+	 * @return
+	 */
+	public CuerpoVibora getCabeza() {
+		if (this.cabeza != null) {
+			return this.cabeza;
+		}
+		return this.cuerpos.get(this.cuerpos.size());
+	}
 
 	/**
 	 * Mueve la vibora hacia el ultimo sentido. Para moverse se elimina la cola y se
 	 * agrega un cuerpo delante de la cabeza
 	 */
 	public void mover() {
+		CuerpoVibora cv = new CuerpoVibora(null);
+		switch (this.sentido) {
+		case Param.POSICION_ESTE:
+			cv = new CuerpoVibora(new Coordenada(this.getCabeza().getCoordenada().getX(),
+					this.getCabeza().getCoordenada().getY() + 1));
+			break;
+		case Param.POSICION_OESTE:
+			cv = new CuerpoVibora(new Coordenada(this.getCabeza().getCoordenada().getX(),
+					this.getCabeza().getCoordenada().getY() - 1));
+			break;
+		case Param.POSICION_NORTE:
+			cv = new CuerpoVibora(new Coordenada(this.getCabeza().getCoordenada().getX() + 1,
+					this.getCabeza().getCoordenada().getY()));
+			break;
+		case Param.POSICION_SUR:
+			cv = new CuerpoVibora(new Coordenada(this.getCabeza().getCoordenada().getX() - 1,
+					this.getCabeza().getCoordenada().getY() + 1));
+			break;
+		}
+		this.cabeza = cv;
+		this.cuerpos.add(cv);
+		this.cuerpos.remove(0);
 
 	}
 
@@ -21,7 +58,8 @@ public class Vibora {
 	 * @param sentido
 	 */
 	public void mover(int sentido) {
-
+		this.sentido = sentido;
+		this.mover();
 	}
 
 	/**
