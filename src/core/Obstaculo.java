c2b56f1bec27a2d9f4739edee3386151d55b35e5
Nicolas Marzullo <marzullonicolas@gmail.com>
2018-10-22 10:19:00 -0300

    Creacion clase obstcaulo
    Se crea la clase obstaculo con una coordenada o un x,y y un largo.

---
+++ src/core/Obstaculo.java
@@ -0,0 +1,18 @@
+package core;
+
+public class Obstaculo {
+	
+	private Coordenada ubicacion;
+	private int largo;
+	
+	public Obstaculo(Coordenada ubicacion, int largo) {
+		this.ubicacion = ubicacion;
+		this.largo = largo;
+	}
+
+	public Obstaculo(int x, int y, int largo) {
+		this.ubicacion = new Coordenada(x, y);
+		this.largo = largo;
+	}
+
+}
