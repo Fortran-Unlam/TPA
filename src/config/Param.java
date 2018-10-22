dbe5a086f8f9c25a470e00b6a2714ab8b107dacf
joniborda <jonathanmatiasborda@gmail.com>
2018-10-22 17:45:36 -0300

    ViboraBot, por ahora solo se mueve random

--- src/config/Param.java
+++ src/config/Param.java
@@ -2,6 +2,6 @@ package config;
 
 public abstract class Param {
 
-	public static final int MAPA_WIDTH = 300;
+	public static final int MAPA_WIDTH = 600;
 	public static final int MAPA_HEIGHT = 600;
 }
