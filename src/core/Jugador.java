3b137712688d5f0cd7d5e713f9dff72c96455734
joniborda <jonathanmatiasborda@gmail.com>
2018-10-23 23:46:18 -0300

    Creo jugador basico

---
+++ src/core/Jugador.java
@@ -0,0 +1,29 @@
+package core;
+
+public class Jugador {
+
+	Vibora vibora;
+	String nombre;
+	
+	public Jugador(Vibora vibora, String nombre) {
+		super();
+		this.vibora = vibora;
+		this.nombre = nombre;
+	}
+
+	public Vibora getVibora() {
+		return vibora;
+	}
+
+	public void setVibora(Vibora vibora) {
+		this.vibora = vibora;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
