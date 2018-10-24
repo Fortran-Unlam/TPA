2c329f11c82b073bde11ccaded10bbc8fedac3e3
joniborda <jonathanmatiasborda@gmail.com>
2018-10-23 23:49:58 -0300

    mas import

--- src/core/mapa/Mapa.java
+++ src/core/mapa/Mapa.java
@@ -4,9 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 import javax.swing.JPanel;
 
