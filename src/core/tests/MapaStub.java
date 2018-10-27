55a6628b479c78b4245117c76d38a05099754bf7
joniborda <jonathanmatiasborda@gmail.com>
2018-10-27 19:38:48 -0300

    Arreglo el new jugador de mapaStub

--- src/core/tests/MapaStub.java
+++ src/core/tests/MapaStub.java
@@ -6,12 +6,15 @@ import core.mapa.Mapa;
 
 public class MapaStub extends Mapa {
 
+	private static int numeroJugador = 0;
+	
 	public MapaStub(int x, int y) {
 		super(x, y);
 	}
 	
 	public boolean add(Vibora vibora) {
-		Jugador jugador = new Jugador(vibora, "as");
+		Jugador jugador = new Jugador(vibora, String.valueOf(numeroJugador));
+		this.numeroJugador++;
 		if (super.add(vibora)) {
 			super.jugadores.add(jugador);
 			return true;
