ed524a8b5ef6df5a05094c7f79a6f82d579222d1
joniborda <jonathanmatiasborda@gmail.com>
2018-10-27 14:49:24 -0300

    mapa uno solo crea frutas y obstaculo

--- src/core/mapa/MapaUno.java
+++ src/core/mapa/MapaUno.java
@@ -3,16 +3,10 @@ package core.mapa;
 import java.util.Random;
 
 import config.Param;
-import config.Posicion;
-import core.Coordenada;
 import core.Obstaculo;
 import core.entidad.Fruta;
-import core.entidad.Vibora;
-import core.entidad.ViboraBot;
 
 public class MapaUno extends Mapa {
-
-	private static final long serialVersionUID = 3707232195274752719L;
 
 	public MapaUno() {
 		super(Param.MAPA_WIDTH / 5, Param.MAPA_HEIGHT / 5);
@@ -28,13 +22,13 @@ public class MapaUno extends Mapa {
 			this.add(new Obstaculo(random.nextInt(Param.MAPA_WIDTH / 5), random.nextInt(Param.MAPA_HEIGHT / 5)));
 		}
 
-		Vibora vibora = new Vibora(new Coordenada(30, 20), 10, Posicion.ESTE);
-		this.add(vibora);
-
-		ViboraBot viboraBot = new ViboraBot(new Coordenada(50, 100));
-		this.add(viboraBot);
-
-		ViboraBot viboraBot2 = new ViboraBot(new Coordenada(100, 70));
-		this.add(viboraBot2);
+//		Vibora vibora = new Vibora(new Coordenada(30, 20), 10, Posicion.ESTE);
+//		this.add(vibora);
+//
+//		ViboraBot viboraBot = new ViboraBot(new Coordenada(50, 100));
+//		this.add(viboraBot);
+//
+//		ViboraBot viboraBot2 = new ViboraBot(new Coordenada(100, 70));
+//		this.add(viboraBot2);
 	}
 }
