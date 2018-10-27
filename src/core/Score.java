c2b73706307f057bea23699120d392843fe7a00f
joniborda <jonathanmatiasborda@gmail.com>
2018-10-27 15:06:32 -0300

    Sigo arrelgnado cosas

--- src/core/Score.java
+++ src/core/Score.java
@@ -5,41 +5,30 @@ import java.util.Collections;
 
 import javax.swing.DefaultListModel;
 
-import core.entidad.Vibora;
-
 public class Score {
-	private ArrayList<Vibora> viboras = new ArrayList<Vibora>();
-	private ArrayList<Puntaje> ranking = new ArrayList<Puntaje>();
-
-	public ArrayList<Puntaje> calcularScore() {
-
-		this.ranking = new ArrayList<Puntaje>();
-
-		for (Vibora vib : this.viboras) {
-			this.ranking.add(new Puntaje(vib.getId(), vib.getFrutasComidas()));
-		}
-
-		Collections.sort(this.ranking);
-
-		return this.ranking;
+	private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+	
+	public ArrayList<Jugador> calcularScore() {
+		
+		Collections.sort(this.jugadores);
+		
+		return this.jugadores;
 	}
 
 	public DefaultListModel<String> ScoreToModel() {
 		this.calcularScore();
 		DefaultListModel<String> modelo = new DefaultListModel<>();
 
-		for (Puntaje p : this.ranking) {
-			modelo.addElement(p.toString());
+		for (Jugador jugador : this.jugadores) {
+			modelo.addElement(jugador.toString());
 		}
 
 		return modelo;
 	}
 
-	public void add(ArrayList<Vibora> viboras) {
-		for (Vibora vibora : viboras) {
-			this.viboras.add(vibora);
+	public void add(ArrayList<Jugador> jugadores) {
+		for (Jugador jugador : jugadores) {
+			this.jugadores.add(jugador);
 		}
-
 	}
-
 }
