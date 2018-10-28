ee884286250f58c64bbd12a15fd4fc727178fd5e
Juan Pablo Pumarega <juanpablopumarega@gmail.com>
2018-10-28 16:25:20 -0300

    Armo listado de salas en curso a modo de ejemplo.
    Seleccionando uno y dando "Unirse" se abre una nueva ventana sala con ese Nombre seleccionado

--- src/cliente/VentanaUnirSala.java
+++ src/cliente/VentanaUnirSala.java
@@ -2,14 +2,19 @@ package cliente;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
 import config.Param;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JList;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 
 public class VentanaUnirSala extends JFrame {
@@ -17,7 +22,11 @@ public class VentanaUnirSala extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private VentanaMenu ventanaMenu;
-
+	@SuppressWarnings("rawtypes")
+	private JList list;
+	public String salaSeleccionada;
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public VentanaUnirSala(VentanaMenu ventanaMenu) {
 		this.ventanaMenu = ventanaMenu;
 		ventanaMenu.setVisible(false);
@@ -41,16 +50,37 @@ public class VentanaUnirSala extends JFrame {
 		lblSalasDisponibles.setBounds(10, 54, 190, 27);
 		contentPane.add(lblSalasDisponibles);
 		
-		JList list = new JList();
-		list.setBounds(12, 98, 454, 209);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(12, 98, 454, 209);
+		contentPane.add(scrollPane);
+		
+		
+		list = new JList();
+		list.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if (arg0.getClickCount() == 1) {
+					salaSeleccionada = ((String) list.getSelectedValue());
+				}
+			}
+		});
+		
+		DefaultListModel salas = new DefaultListModel();
+		salas.addElement("Sala 1");
+		salas.addElement("Sala 2");
+		salas.addElement("Sala 3");
+		list.setModel(salas);
 		contentPane.add(list);
+		scrollPane.setViewportView(list);		
 		//Agregar lista de salas. Ver de donde viene la lista.
 		
 		JButton btnUnirse = new JButton("Unirse");
 		btnUnirse.setBounds(68, 309, Param.BOTON_WIDTH, Param.BOTON_HEIGHT);
 		btnUnirse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				abrirVentanaSala();
+				abrirVentanaSala(salaSeleccionada);
 			}
 		});
 		contentPane.add(btnUnirse);
@@ -76,7 +106,7 @@ public class VentanaUnirSala extends JFrame {
 		contentPane.add(btnRefrescarSalas);
 	}
 	
-	private void abrirVentanaSala() {
+	private void abrirVentanaSala(String salaSeleccionada) {
 		new VentanaSala(this).setVisible(true);
 	}
 	
