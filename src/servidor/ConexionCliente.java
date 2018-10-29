08d4df855d6dc705cefc42d048ea136f0430bae7
Nicolas Marzullo <marzulloncolas@gmail.com>
2018-10-29 16:07:30 -0300

    NICO 1 HIBERNATE 0 DALE BOCA

--- src/servidor/ConexionCliente.java
+++ src/servidor/ConexionCliente.java
@@ -75,7 +75,7 @@ public class ConexionCliente extends Thread {
 						tx.commit();
 						
 						Query queryLogueo = session.createQuery(
-								"SELECT * FROM Usuario WHERE username = " + username + "AND " + "password= " + password);
+								"SELECT u FROM Usuario u WHERE u.username = " + username + "AND " + "u.password= " + password);
 						
 						List<Usuario> usuarios = queryLogueo.getResultList();
 						
