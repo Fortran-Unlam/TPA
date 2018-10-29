868a7d043961f226c93f8b5c749a1652c6398727
joniborda <jonathanmatiasborda@gmail.com>
2018-10-29 17:53:21 -0300

    Clase Message para la comunicacion entre cliente y servidor

---
+++ src/servidor/Messasge.java
@@ -0,0 +1,29 @@
+package servidor;
+
+public class Messasge {
+
+	private String type;
+	private Object data;
+
+	public Messasge(String type, Object data) {
+		super();
+		this.type = type;
+		this.data = data;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+}
