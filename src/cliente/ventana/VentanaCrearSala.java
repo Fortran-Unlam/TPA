0520886e1e334f78c302db6a5b0ae86defaf1f78
Juan Pablo Pumarega <juanpablopumarega@gmail.com>
2018-10-31 11:35:16 -0300

    Quito import innecesario

--- src/cliente/ventana/VentanaCrearSala.java
+++ src/cliente/ventana/VentanaCrearSala.java
@@ -4,9 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
