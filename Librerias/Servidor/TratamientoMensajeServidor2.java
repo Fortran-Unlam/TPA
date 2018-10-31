5d6f57acd6aea902a5da6a4d5aa995b1e3320253
Reflejo <jtocino@gylgroup.com>
2018-10-30 22:18:24 -0300

    Solución al problema no se encontró o cargo la clase principal.
    Ya debería funcionar correctamente esta rama.

--- Librerias/Servidor/TratamientoMensajeServidor2.java
+++ Librerias/Servidor/TratamientoMensajeServidor2.java
@@ -5,7 +5,10 @@ import Entidades.Usuario;
 import Entidades.UsuarioHibernate;
 import Seguridad.Seguridad;
 
-public class TratamientoMensajeServidor 
+/* Reflejo 30/10 le cambio el nombre a TratamientoMensajeServidor2 porque ya existe TratamientoMensajeServidor
+ * https://github.com/Fortran-Unlam/TPA/commit/4fd27fb74ffd3b20695f0ea113554f4b137d1283
+ */
+public class TratamientoMensajeServidor2 
 {
 	public static String tratarMensaje(String mensajeRecibido)
 	{
