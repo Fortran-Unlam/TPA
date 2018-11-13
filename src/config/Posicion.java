c4e7f97b6b34d322054152c05e3be65e3743fa4d
jborda <jborda@jus.gov.ar>
2018-11-13 10:33:50 -0300

    Agrego function en posicion

--- src/config/Posicion.java
+++ src/config/Posicion.java
@@ -1,8 +1,19 @@
 package config;
 
 public enum Posicion {
-	ESTE,
-	SUR,
-	OESTE,
-	NORTE
+	ESTE, SUR, OESTE, NORTE;
+
+	public int rotacion() {
+		switch (this) {
+		case OESTE:
+			return 0;
+		case SUR:
+			return 90;
+		case ESTE:
+			return 180;
+		case NORTE:
+			return 270;
+		}
+		return 0;
+	}
 }
