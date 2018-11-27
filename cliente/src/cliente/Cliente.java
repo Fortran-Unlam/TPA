637017796df7a4b94d49bb56ec8c276f63826b4f
joniborda <jonathanmatiasborda@gmail.com>
2018-11-27 19:25:54 -0300

    Agrego logger

--- cliente/src/cliente/Cliente.java
+++ cliente/src/cliente/Cliente.java
@@ -2,10 +2,16 @@ package cliente;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Calendar;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import cliente.ventana.Sincronismo;
 import cliente.ventana.usuario.VentanaLoginUsuario;
 import config.Param;
+import javassist.bytecode.stackmap.TypeData.ClassName;
 
 public class Cliente {
 
@@ -16,13 +22,24 @@ public class Cliente {
 	private static ConexionServidor conexionServidor;
 	private static ConexionServidorBackOff conexionServidorBackOff;
 	private static Sincronismo sincronismo;
-	
+	private static final Logger LOGGER = Logger.getAnonymousLogger();
+
 	public static void main(String[] args) {
 		new Cliente();
 	}
 
 	public Cliente() {
 		try {
+			FileHandler fh = null;
+			try {
+	            fh = new FileHandler("logger.log", true);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
+	        fh.setFormatter(new SimpleFormatter());
+	        LOGGER.addHandler(fh);
+	        
 			socketOut = new Socket(Param.HOST, Param.PORT_1);
 			socketIn = new Socket(Param.HOST, Param.PORT_2);
 
@@ -34,12 +51,12 @@ public class Cliente {
 
 			conexionServidorBackOff.start();
 			sincronismo = new Sincronismo();
-			
+
 			VentanaLoginUsuario login = new VentanaLoginUsuario();
 			login.setVisible(true);
-			
+
 		} catch (IOException ex) {
-			System.out.println("No se ha podido conectar con el servidor (" + ex.getMessage() + ").");
+			LOGGER.log(Level.SEVERE, "No se ha podido conectar con el servidor (" + ex.getMessage() + ").");
 		}
 	}
 
@@ -54,7 +71,7 @@ public class Cliente {
 	public static ConexionServidorBackOff getconexionServidorBackOff() {
 		return conexionServidorBackOff;
 	}
-	
+
 	public static Sincronismo getSincronismo() {
 		return sincronismo;
 	}
