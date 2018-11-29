29db87554e685a1b99fca4f977214d1993672076
jborda <jborda@jus.gov.ar>
2018-11-29 10:32:16 -0300

    Si lo vas a usar para ocultarlo, ocultalo cuando lo llamas (unir Sala)

--- cliente/src/cliente/ventana/VentanaSala.java
+++ cliente/src/cliente/ventana/VentanaSala.java
@@ -58,12 +58,10 @@ public class VentanaSala extends JFrame {
 	private JComboBox<Object> cantidadDeFrutascomboBox;
 	private JComboBox<Object> cantidadDeBotsComboBox;
 
-	public VentanaSala(VentanaMenu ventanaMenu, VentanaUnirSala ventanaUnirSala, boolean admin, String nombreSala) {
+	public VentanaSala(VentanaMenu ventanaMenu, boolean admin, String nombreSala) {
 		this.ventanaMenu = ventanaMenu;
 		this.nombreSala = nombreSala;
 		this.ventanaMenu.setVisible(false);
-		if (ventanaUnirSala != null)
-			ventanaUnirSala.setVisible(false);
 		this.visibiliadAdmin = admin;
 		this.setearComponentes();
 		this.addListener();
