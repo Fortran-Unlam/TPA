16e40cc5db328a55b006e46ca98d374e335a5c8d
jborda <jborda@jus.gov.ar>
2018-11-29 07:50:56 -0300

    lo rompi

--- cliente/src/cliente/input/Input.java
+++ cliente/src/cliente/input/Input.java
@@ -5,16 +5,16 @@ import config.Posicion;
 public class Input  {
 
 	public Teclado teclado = new Teclado();
-	public Joystick joystick = new Joystick();
+//	public Joystick joystick = new Joystick();
 	private DispositivoEntrada dispositivo = null;
 	
 	public Input() {
-		if (joystick.isActive()) {
-			joystick.run();
-			this.dispositivo = joystick;
-		} else {
-			this.dispositivo = teclado;
-		}
+//		if (joystick.isActive()) {
+//			joystick.run();
+//			this.dispositivo = joystick;
+//		} else {
+//			this.dispositivo = teclado;
+//		}
 	}
 	
 	public Posicion getUltimaTecla() {
