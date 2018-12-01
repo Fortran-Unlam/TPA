3a8a37fa25a0eccb1740729346d43f8b5e71264b
joniborda <jonathanmatiasborda@gmail.com>
2018-12-01 04:15:48 -0300

    no se usa input

--- cliente/src/cliente/input/Input.java
+++
@@ -1,6 +0,0 @@
-package cliente.input;
-
-public class Input {
-
-	public Teclado teclado = new Teclado();
-}
--- cliente/src/cliente/ventana/VentanaJuego.java
+++ cliente/src/cliente/ventana/VentanaJuego.java
@@ -29,7 +29,7 @@ import cliente.Cliente;
 import cliente.Imagen;
 import cliente.Sonido;
 import cliente.Usuario;
-import cliente.input.Input;
+import cliente.input.Teclado;
 import config.Param;
 import config.Posicion;
 
@@ -359,7 +359,7 @@ public class VentanaJuego extends JFrame {
 			}
 		});
 
-		this.addKeyListener(new Input().teclado);
+		this.addKeyListener(new Teclado());
 
 		this.btnSalirJuego.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
